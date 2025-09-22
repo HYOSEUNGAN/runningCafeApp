789ff197de5c1087aaa7cce4e8dfaf47540d801d
HYOSEUNGAN <ace.aaiden@gmail.com>
2025-09-22 13:45:30 +0900

    supabase set

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,16 +1,16 @@
-// import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js';
 
-// const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-// const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
-// if (!supabaseUrl || !supabaseAnonKey) {
-//   throw new Error('Supabase URL과 Anon Key가 환경변수에 설정되어야 합니다.');
-// }
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Supabase URL과 Anon Key가 환경변수에 설정되어야 합니다.');
+}
 
-// export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-//   auth: {
-//     autoRefreshToken: true,
-//     persistSession: true,
-//     detectSessionInUrl: true,
-//   },
-// });
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: true,
+  },
+});
